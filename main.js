532b351e4eabb7f6cc0819814623cd2097dae917
Yikuan Sun <yikuansun2015@gmail.com>
2021-02-06 16:09:55 -0500

    base files

---
+++ main.js
@@ -0,0 +1,25 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    webPreferences: {
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+  mainWindow.maximize();
+
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length == 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () { app.quit() });
