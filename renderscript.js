f98a7e4246b2249f427139b917c549804cb872b8
Yikuan Sun <yikuansun2015@gmail.com>
2021-02-06 19:31:59 -0500

    Update renderscript.js

--- renderscript.js
+++ renderscript.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
-setTimeout(function() { document.querySelector("#loadingscreen").remove(); }, 5000);
-
 request = new XMLHttpRequest();
 request.open("GET", "https://raw.githubusercontent.com/yikuansun/photopeaenvironment/master/environment.json", false);
 request.send();
 options = request.responseText;
+
+setTimeout(function() { document.querySelector("#loadingscreen").remove(); }, 5000);
 
 document.querySelector("iframe").src = "https://www.photopea.com#" + encodeURI(options);
