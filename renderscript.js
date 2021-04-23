67c336e82aff7b7f511fc46960fca107fcdaa66a
Yikuan Sun <yikuansun2015@gmail.com>
2021-04-23 16:41:53 -0400

    Use my API simplification package

--- renderscript.js
+++ renderscript.js
@@ -1,8 +1,8 @@
-request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
 request.open("GET", "https://raw.githubusercontent.com/yikuansun/photopeaenvironment/master/environment.json", false);
 request.send();
 options = request.responseText;
 
 setTimeout(function() { document.querySelector("#loadingscreen").remove(); }, 5000);
 
-document.querySelector("iframe").src = "https://www.photopea.com#" + encodeURI(options);
+Photopea.initEmbed(document.querySelector("div"), options);
